f9d00726aaac31be15a50498cc7a3976fcfc6bc0
lilkam2543 <kjackson62@atu.edu>
2025-01-23 15:30:06 -0600

    Update practice_Data.cpp

--- practice_Data.cpp
+++ practice_Data.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
     cout << "Hello World!\n";
-    court << "pulling functions\n";
+    court << "pulling functions in github\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
